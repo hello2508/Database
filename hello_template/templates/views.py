0c87b92d52c9253cc6c773baa77e86cb7face8e7
abi <abenayabv@gmail.com>
2019-10-31 15:41:26 +0800

    deleted hello_template

--- hello_template/templates/views.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, Blueprint
-
-medium_blueprint = Blueprint('medium_article',__name__) #creates a blueprint for our medium article
-@medium_blueprint.route('/articlelist') #listens to URL /articlelist and renders a template file index.html
-def index():
- return render_template("index.html")
