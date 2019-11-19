88c86c914f5ae07dcf72975a55d3fa1d8c24d793
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2019-11-20 00:49:04 +0800

    mysql form in progress

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,64 +1,36 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-# from flask_sqlalchemy import SQLAlchemy
-import MySQLdb
-
-######
-import os
-#####
+import yaml
 
 app = Flask(__name__)
 
-# db configuration required by flask
-app.config['MYSQL_HOST'] ='localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'sutd1234'
-app.config['MYSQL_DB'] = 'dbds.kindle_reviews'
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
 
-# create an instance which will provide access
+# instantiate an object for MySQL
 mysql = MySQL(app)
 
-@app.route("/")
-def home():
-	# establish connection
-	cur = mysql.connection.cursor()
-	cur.execute("SELECT asin FROM dbds.kindle_reviews LIMIT 10") # execute query
-	# for row in cur.fetchall():
-	# 	print row[1]
-	# fetchdata = cur.fetchall() # fetch(one/many) data from 'dbds.kindle_reviews' table
-	cur.close()
-
-	return render_tamplate('home.html',data="nodata1")
-
-@app.route("/test")
-def test_route():
-	user_details = {
-		'asin':'B000F83SZQ',
-		'reviewerID':'A1F6404F1VG29J'
-	}
-	#####
-	print("current dir: {0}".format(os.getcwd()))
-	####
-
-	return render_template('home.html', user="user_details")
-	# return (user_details)
-
-# Login Page
-# @app.route('/login', methods=["GET","POST"])
-# def login_page():
-# 	try:
-
-# 	except Exception as e:
-# 		flash(e) #display error
-# 		return render_template("login.html", error=error)
-
-# 	return render_template("login.html")
-
-# Error handling
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template("404.html")
-
+# GET request- display orm to the user
+# POSt request - store details onto the database
+@app.route("/", methods=['GET','POST'])
+def index():
+	if request.method == 'POST':
+		# Fetch form data
+		userDetails = request.form
+		name = userDetails['name']
+		book_title = userDetails['book_title']
+		review = userDetails['review']
+		cur = mysql.connection.cursor()
+		cur.execute("INSERT INTO test(reviewerName,booktitle,reviewText) VALUES(%s, %s, %s)",(name,book_title,review) )
+		# Save changes into the database
+		mysql.connection.commit()
+		cur.close()
+		return 'success'
+	return render_template('home.html')
 
 
 if __name__ == "__main__":
