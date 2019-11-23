0803e09e856648cc248b46bfa28917c20e4b6d2d
Bertha <bertha_han@mymail.sutd.edu.sg>
2019-11-23 23:15:53 +0800

    add example in flaskproject folder

--- flaskproject/__init__.py
+++ flaskproject/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
 from flaskproject.database import DB
+from flaskproject.models.job import Job
 
 def create_app(config):
     app = Flask(__name__)
     DB.init()
     register_blueprints(app)
+    for job_name in ['job1','job2','job3']:
+        new_job = Job(name=job_name)
+        new_job.insert()
     return app
 
 
