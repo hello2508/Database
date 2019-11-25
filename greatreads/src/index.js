e7f3fe0c809b08451a4684b5e06da6f8a3782bc9
abi <abenayabv@gmail.com>
2019-11-25 16:20:23 +0800

    added react router

--- greatreads/src/index.js
+++ greatreads/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Menu from "./components/Menu";
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<Menu />, document.getElementById('menu'));
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
