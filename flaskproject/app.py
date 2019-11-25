f0385b6abd5d71403e46a1e6659a18d227ee9c03
Bertha <bertha_han@mymail.sutd.edu.sg>
2019-11-25 16:34:04 +0800

    added cat page to flaskproj

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)  #creates an app
 def webprint():
     return render_template('hompage.html')
 
+@app.route('/category')
+def categorypage():
+    return render_template('categorypage2.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
