21a9d4ec09b4dbbfc6c73d599e0f74b65f2f2978
nixster <iamnixster@gmail.com>
2019-11-29 15:10:39 +0800

    added monstercat html to test out displaying stuff from mongo

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,33 +1,34 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 
 app = Flask(__name__)  #creates an app
 
-mongo_store = PyMongo(app, uri="mongodb://18.141.0.98/goodread")
-
+# mongo_store = PyMongo(app, uri="mongodb://18.141.0.98/goodread")
+mongo_store = PyMongo(app, uri="mongodb://localhost:27017/nezukodb")
+metadata = mongo_store.db.metadata
+# logs = mongo_store.logs
 
 @app.route('/')
 def webprint():
     return render_template('hompage.html')
 
-
-
-@app.route("/monmon")
-def mongostuff():
-    # standuser = mongo.db.books
-    nezuko = mongo_store.db.metadata
+@app.route("/monstercat/<category>")
+def monstercat(category):
     # name = standuser.find_one({"name": "Giss"})
     # results = standuser.find({"name": "some book"})
     # demons = nezuko.distinct('categories')
     # demons = nezuko.find('png')
     # demons = nezuko.distinct('categories')
-    demons = nezuko.distinct('categories')
+    # demons = nezuko.distinct('categories')
 
     newarr = []
 
-    # db.metadata.find({'categories': {$elemMatch: {$elemMatch: {$eq: "Reference"}}}}).count()
-    # for x in uniques:
+    # categories = metadata.find({'categories': {"$elemMatch": {"$elemMatch": {"$eq": category} } }}, {'asin': 1, '_id': 0 })
+    categories = metadata.find({'categories': {"$elemMatch": {"$elemMatch": {"$eq": category} } }}, {'imUrl': 1, '_id': 0 })
+
+    # for x in categories:
+    #     newarr.append(x['asin'])
 
     # Method 2 of printing Cursor stuff
     # blah = ""
@@ -39,7 +40,11 @@ def mongostuff():
     # dump(results) to ddeal with Cursor
 
 
-    return dumps(demons)
+    return render_template('monstercat.html', categories=categories)
+    #     f'''
+    #     <h1>{category}</h1>
+    #     <img src="{metadata[newarr[0]].imUrl}" >
+    # '''
 
 @app.route('/category')
 def categorypage():
